4e238e1c98e8d075ac75b58a16f0f43139d447c2
Alena Simoshina <alenasimoshina@yandex.ru>
2022-01-28 13:17:47 +0300

    исправить ошибки

--- index.js
+++ index.js
@@ -8,27 +8,28 @@ likeButton.forEach(function(likeButton) {
 
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
-const popupCloseButton = document.querySelector('.popup__exit-button')
-
-popupOpenButton.addEventListener('click', function(event) {
-  popup.classList.add('popup_opened');
-  event.preventDefault();
-});
-
-popupCloseButton.addEventListener('click', function(event) {
-  popup.classList.remove('popup_opened');
-  event.preventDefault();
-});
+const popupCloseButton = document.querySelector('.popup__exit-button');
 
 let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('input[name="name"]');
 let aboutInput = formElement.querySelector('input[name="about"]');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+function popupForm (evt) {
+  evt.preventDefault();
+  popup.classList.toggle('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  aboutInput.value = profileSubtitle.textContent;
+};
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  document.querySelector('.profile__title').textContent = nameInput.value;
-  document.querySelector('.profile__subtitle').textContent = aboutInput.value;
-  popup.classList.remove('popup_opened');
-}
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = aboutInput.value;
+  popupForm (evt);
+};
 
+popupOpenButton.addEventListener('click', popupForm);
+popupCloseButton.addEventListener('click', popupForm);
 formElement.addEventListener('submit', formSubmitHandler); 
